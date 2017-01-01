42582d09fc097f9b9d78f45134ec38119285db8b
onevcat <onevcat@gmail.com>
2017-01-01 20:40:48 +0900

    Move AddressParser a standalone

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Hedwig",
     dependencies: [
         .Package(url: "https://github.com/vapor/tls.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 1),
+        .Package(url: "https://github.com/onevcat/AddressParser.git", majorVersion: 1)
     ]
 )
